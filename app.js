5f2a90177493dab098ffc69a2b3917bd12dee62a
sonhoang <doremon.dm43@gmail.com>
2022-04-30 18:38:14 +0700

    30/4/2022

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const app = express();
 const PORT = 5000;
+let formData = require('./data.js');
 
 app.use(express.static('public'));
 app.use(express.urlencoded())
 
 app.post('/login', (req, res) => {
-    const {name, role, date, comment} = req.body;
-    console.log(name, role, date, comment)
+    let {name, role, date, comment} = req.body;
+    formData = {
+        Name: name, 
+        Menu: role, 
+        date: date, 
+        Feedback: comment
+    };
+    console.log(formData);
     res.send('send success');
 })
-
+console.log(formData);
 app.listen(PORT, () => {
     console.log(`server is listening on port ${5000}`);
 })
---
+++ data.js
@@ -0,0 +1,3 @@
+const formData = [];
+
+module.exports = {formData};
